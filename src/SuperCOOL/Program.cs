c0b1140ee4e63589dd04e611647877443acc46c4
Jose10go <Jose10go2017@gmail.com>
2019-03-01 15:22:24 -0500

    Ready To Start

--- src/SuperCOOL/Program.cs
+++ src/SuperCOOL/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using SuperCOOL.ANTLR;
+using System;
 
 namespace SuperCOOL
 {
-    class Program
+    class Compiler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string program = args[0];
+            string program = "";
+            SuperCOOLLexer superCOOLLexer = new SuperCOOLLexer(new AntlrInputStream(program));
+            SuperCOOLParser superCOOLParser = new SuperCOOLParser(new CommonTokenStream(superCOOLLexer));
         }
     }
 }
