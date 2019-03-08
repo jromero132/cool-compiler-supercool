380f58d085de79b5b86973e53a8b1be83a29b795
Jose10go <Jose10go2017@gmail.com>
2019-03-08 14:32:48 -0500

    Improving Compiler Pipeline

--- src/SuperCOOL/Program.cs
+++ src/SuperCOOL/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using SuperCOOL.ANTLR;
+using SuperCOOL.Core;
 using System;
+using System.IO;
 
 namespace SuperCOOL
 {
@@ -8,10 +11,13 @@ namespace SuperCOOL
     {
         static void Main(string[] args)
         {
-            //string program = args[0];
-            string program = "";
+            //string program = "";
+            FileStream program = new FileStream(Directory.GetCurrentDirectory()+"/../../../"+"/Examples/atoi.cl",FileMode.Open);
             SuperCOOLLexer superCOOLLexer = new SuperCOOLLexer(new AntlrInputStream(program));
             SuperCOOLParser superCOOLParser = new SuperCOOLParser(new CommonTokenStream(superCOOLLexer));
+            IParseTree parseTree = superCOOLParser.program();
+            CompilationUnit compilationUnit = new CompilationUnit();
+            parseTree.Accept(new SemanticCheck.SuperCoolSemanticCheckVisitor(compilationUnit));
         }
     }
 }
