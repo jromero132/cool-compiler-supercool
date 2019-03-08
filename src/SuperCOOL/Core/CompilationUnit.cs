ba82b161cfb15d6e36e362dcccbbdfecbc540f75
Jose10go <Jose10go2017@gmail.com>
2019-03-08 14:31:58 -0500

    Compilation Unit and CoolType Basics

---
+++ src/SuperCOOL/Core/CompilationUnit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperCOOL.Core
+{
+    public class CompilationUnit
+    {
+        HashSet<CoolType> Types { get; set; }
+
+        public bool IsTypeDef(string Name)
+        {
+            return Types.Contains(new CoolType(Name));
+        }
+
+        public bool InheritsFrom(CoolType A, CoolType B)
+        {
+            return A.IsIt(B);
+        }
+
+        public CoolType GetTypeIfDef(string Name)
+        {
+            CoolType ret;
+            Types.TryGetValue(new CoolType(Name), out ret);
+            return ret;
+        }
+
+        public bool NotCyclicalInheritance()
+        {
+            return true;//TODO: Verify if CoolType Tree is a DAG
+        }
+
+        public bool HasEntryPoint()
+        {
+            return true;//TODO: Verify if there is an entry Point;
+        }
+    }
+}
---
+++ src/SuperCOOL/Core/CoolType.cs
@@ -0,0 +1,38 @@
+﻿namespace SuperCOOL.Core
+{
+    public class CoolType
+    {
+        public CoolType Parent { get; private set; }
+        public string Name { get; private set; }
+
+        public CoolType(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public CoolType(string Name,CoolType Parent):this(Name)
+        {
+            this.Parent = Parent;
+        }
+
+        public override bool Equals(object obj)
+        {
+            CoolType coolType = obj as CoolType;
+            return coolType.Name == Name;
+        }
+
+        public bool IsIt(CoolType Tatara)
+        {
+            var type = this;
+            if (type == Tatara)
+                return true;
+            while (type.Parent!=null)
+            {
+                type = type.Parent;
+                if (type.Equals(Tatara)) return true;
+            }
+            return false;
+        }
+
+    }
+}
