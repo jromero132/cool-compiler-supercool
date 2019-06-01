44b09af8122fd605417ad9652f65c4a9bcd0066f
jyeros <jyero96@gmail.com>
2019-06-01 16:24:44 -0400

    add func invoke nodes

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILFuncStaticCallNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILFuncStaticCallNode : ASTCILExpressionNode
+    {
+        public string MethodName { get; }
+        public string Type { get; }
+        public IReadOnlyList<ASTCILExpressionNode> Arguments { get; }
+
+        public ASTCILFuncStaticCallNode(string methodName, string type, IEnumerable<ASTCILExpressionNode> arguments)
+        {
+            MethodName = methodName;
+            Type = type;
+            Arguments = arguments.ToList();
+        }
+    }
+}
