89e7d9480c33925f2ee63a6a332b42004f2d0741
jyeros <jyero96@gmail.com>
2019-05-31 23:52:30 -0400

    add cil type node

--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILTypeNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILTypeNode.cs
@@ -2,7 +2,18 @@
 
 namespace SuperCOOL.CodeGeneration.CIL.AST
 {
-    class ASTCILTypeNode : ASTCILDataNode
+    class ASTCILTypeNode
     {
+        public int AllocateSize { get; }
+        public IReadOnlyDictionary<string, int> AttributesOffset { get; }
+        public IReadOnlyDictionary<string, int> MethodsOffset { get; }
+
+        public ASTCILTypeNode(int allocateSize, IReadOnlyDictionary<string, int> attributesOffset,
+            IReadOnlyDictionary<string, int> methodsOffset)
+        {
+            AllocateSize = allocateSize;
+            AttributesOffset = new Dictionary<string, int>(attributesOffset);
+            MethodsOffset = new Dictionary<string, int>(methodsOffset);
+        }
     }
 }
