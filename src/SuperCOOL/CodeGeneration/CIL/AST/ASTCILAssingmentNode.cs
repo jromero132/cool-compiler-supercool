65055ce179057bcbf9cf8d666f0b649d6bb7882d
jyeros <jyero96@gmail.com>
2019-06-01 14:10:58 -0400

    add assignment node

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILAssingmentNode.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILAssingmentNode : ASTCILExpressionNode
+    {
+        public ASTCILIdNode Id { get; }
+        public ASTCILExpressionNode Expresion { get; }
+
+        public ASTCILAssingmentNode(ASTCILIdNode id, ASTCILExpressionNode expresion)
+        {
+            Id = id;
+            Expresion = expresion;
+        }
+    }
+}
