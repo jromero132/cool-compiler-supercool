b250324a8e650279f05c6a22b4e2f280bd7e1fff
jyeros <jyero96@gmail.com>
2019-06-01 16:04:51 -0400

    add attribute nodes

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILGetAttrNode.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILGetAttrNode : ASTCILExpressionNode
+    {
+        public string TypeName { get; }
+        public string AttributeName { get; }
+
+        public ASTCILGetAttrNode(string typeName, string attributeName)
+        {
+            TypeName = typeName;
+            AttributeName = attributeName;
+        }
+    }
+}
