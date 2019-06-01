b575f7337d160325b6ab57c533e5654f2f07044f
Jose Ariel Romero <josea132.romero@gmail.com>
2019-05-31 23:56:03 -0400

    Added lca of types.

--- src/SuperCOOL/Core/CompilationUnit.cs
+++ src/SuperCOOL/Core/CompilationUnit.cs
@@ -5,24 +5,22 @@ namespace SuperCOOL.Core
 {
     public class CompilationUnit
     {
-        HashSet<CoolType> Types { get; set; }
+        private Dictionary<CoolType, List<CoolType>> lca_table;
+        private Dictionary<CoolType, int> distance;
+
+        public CoolType ObjectType => GetTypeIfDef( "object" );
+
+        public HashSet<CoolType> Types;
 
         HashSet<CoolMethod> Method { get; set; }
 
-        public bool IsTypeDef(string Name)
-        {
-            return Types.Contains(new CoolType(Name));
-        }
+        public bool IsTypeDef( string Name ) => this.Types.Contains( new CoolType( Name ) );
 
-        public bool InheritsFrom(CoolType A, CoolType B)
-        {
-            return A.IsIt(B);
-        }
+        public bool InheritsFrom( CoolType A, CoolType B ) => A.IsIt( B );
 
-        public CoolType GetTypeIfDef(string Name)
+        public CoolType GetTypeIfDef( string Name )
         {
-            CoolType ret;
-            Types.TryGetValue(new CoolType(Name), out ret);
+            this.Types.TryGetValue( new CoolType( Name ), out var ret );
             return ret;
         }
 
@@ -30,6 +28,7 @@ namespace SuperCOOL.Core
         {
             return true;//TODO: Verify Types NotCyclicalInheritance and Entry Point not inheritance from bool int string ...all this semantics goes here
         }
+
         private bool NotCyclicalInheritance()
         {
             return true;//TODO: Verify if CoolType Tree is a DAG
@@ -40,9 +39,83 @@ namespace SuperCOOL.Core
             return true;//TODO: Verify if there is an entry Point;
         }
 
-        internal CoolType GetTypeLCA(CoolType type1, CoolType type2)
+        public CoolType GetTypeLCA( CoolType type1, CoolType type2 )
         {
-            throw new NotImplementedException();//TODO: MAKE LCA
+            if( this.lca_table == null )
+                LCATable();
+
+            int l1 = this.distance[ type1 ], l2 = this.distance[ type2 ];
+            if( l1 > l2 )
+            {
+                var temp1 = type1;
+                type1 = type2;
+                type2 = temp1;
+
+                var temp2 = l1;
+                l1 = l2;
+                l2 = temp2;
+            }
+
+            for( int i = (int)Math.Log( l2, 2 ) ; i >= 0 ; --i )
+                if( l2 - ( 1 << i ) >= l1 )
+                {
+                    type2 = this.lca_table[ type2 ][ i ];
+                    l2 = this.distance[ type2 ];
+                }
+
+            if( type1 == type2 )
+                return type1;
+
+            for( int i = (int)Math.Log( l1, 2 ) ; i >= 0 ; --i )
+            {
+                var t1 = this.lca_table[ type1 ];
+                var t2 = this.lca_table[ type2 ];
+
+                if( t1[ i ] != null && t1[ i ] != t2[ i ] )
+                {
+                    type1 = this.lca_table[ type1 ][ i ];
+                    type2 = this.lca_table[ type2 ][ i ];
+                }
+            }
+            return this.lca_table[ type1 ][ 0 ];
+        }
+
+        private void LCATable()
+        {
+            this.lca_table = new Dictionary<CoolType, List<CoolType>>()
+            {
+                [ this.ObjectType ] = new List<CoolType>() { null }
+            };
+            this.distance = new Dictionary<CoolType, int>()
+            {
+                [ this.ObjectType ] = 1
+            };
+
+            Queue<CoolType> q = new Queue<CoolType>();
+            for( q.Enqueue( this.ObjectType ) ; q.Count > 0 ; q.Dequeue() )
+            {
+                var now = q.Peek();
+                foreach( var child in now.Childs )
+                {
+                    if( !this.lca_table.ContainsKey( child ) )
+                    {
+                        this.lca_table[ child ] = new List<CoolType> { now };
+                        this.distance[ child ] = this.distance[ now ] + 1;
+                        q.Enqueue( child );
+                    }
+                }
+            }
+
+            for( int j = 1 ; ( 1 << j ) <= this.lca_table.Count ; ++j )
+            {
+                foreach( var pair in this.lca_table )
+                {
+                    var parent = pair.Value[ j - 1 ];
+                    var parents_table = parent is null ? null : this.lca_table[ parent ];
+                    var next_parent = parents_table?[ j - 1 ];
+                    pair.Value.Add( next_parent );
+                }
+            }
         }
     }
 }
--- src/SuperCOOL/Core/CoolType.cs
+++ src/SuperCOOL/Core/CoolType.cs
@@ -1,43 +1,38 @@
-﻿namespace SuperCOOL.Core
+﻿using System.Collections.Generic;
+
+namespace SuperCOOL.Core
 {
     public class CoolType
     {
         public CoolType Parent { get; private set; }
+        public List<CoolType> Childs { get; set; }
+
         public string Name { get; private set; }
 
-        public CoolType(string Name)
-        {
-            this.Name = Name;
-        }
+        public CoolType( string Name ) => this.Name = Name;
 
-        public CoolType(string Name,CoolType Parent):this(Name)
-        {
-            this.Parent = Parent;
-        }
+        public CoolType( string Name, CoolType Parent ) : this( Name ) => this.Parent = Parent;
 
-        public override bool Equals(object obj)
+        public override bool Equals( object obj )
         {
             CoolType coolType = obj as CoolType;
-            return coolType.Name == Name;
+            return coolType.Name == this.Name;
         }
 
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode();
-        }
+        public override int GetHashCode() => this.Name.GetHashCode();
 
-        public bool IsIt(CoolType Tatara)
+        public bool IsIt( CoolType Tatara )
         {
             var type = this;
-            if (type == Tatara)
+            if( type == Tatara )
                 return true;
-            while (type.Parent!=null)
+            while( type.Parent != null )
             {
                 type = type.Parent;
-                if (type.Equals(Tatara)) return true;
+                if( type.Equals( Tatara ) )
+                    return true;
             }
             return false;
         }
-
     }
 }
