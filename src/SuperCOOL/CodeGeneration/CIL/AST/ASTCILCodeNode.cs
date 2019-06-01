1c290b06619d0418c0b12ea84c0bbc63b2d74135
jyeros <jyero96@gmail.com>
2019-06-01 02:33:18 -0400

    add basic code and data

--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILCodeNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILCodeNode.cs
@@ -1,6 +1,15 @@
-﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+﻿using System.Collections.Generic;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
 {
     class ASTCILCodeNode : ASTCILNode
     {
+        public IReadOnlyList<ASTCILFuncNode> Functions { get; }
+
+        public ASTCILCodeNode(IReadOnlyList<ASTCILFuncNode> functions)
+        {
+            Functions = functions;
+        }
+        //TODO add default method to Object, String, IO, GarbajeCollector
     }
 }
--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILDataNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILDataNode.cs
@@ -1,6 +1,16 @@
-﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
 {
     class ASTCILDataNode : ASTCILNode
     {
+        public IReadOnlyList<ASTCILTypeNode> Types { get; }
+
+        public ASTCILDataNode(IEnumerable<ASTCILTypeNode> types)
+        {
+            Types = types.ToList();
+        }
+        // TODO add list null and others data 
     }
 }
