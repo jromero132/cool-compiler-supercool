ee09176750dbe5c3d5d474952da21ca2261b79a7
jyeros <jyero96@gmail.com>
2019-06-01 14:11:45 -0400

    add block node

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILBlockNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILBlockNode : ASTCILExpressionNode
+    {
+        public IReadOnlyList<ASTCILExpressionNode> Expressions { get; }
+
+        public ASTCILBlockNode(IEnumerable<ASTCILExpressionNode> expressions)
+        {
+            Expressions = expressions.ToList();
+        }
+    }
+}
