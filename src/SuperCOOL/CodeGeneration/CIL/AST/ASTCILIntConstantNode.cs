526a845b2d2f74ad1fa57fa39706d277d09d8e2b
jyeros <jyero96@gmail.com>
2019-06-01 15:43:51 -0400

    add constants

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIntConstantNode.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILIntConstantNode : ASTCILExpressionNode
+    {
+        public int Value { get; }
+
+        public ASTCILIntConstantNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILStringConstantNode.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILStringConstantNode : ASTCILNode
+    {
+        public string Value { get; }
+
+        public ASTCILStringConstantNode(string value)
+        {
+            Value = value;
+        }
+    }
+}
