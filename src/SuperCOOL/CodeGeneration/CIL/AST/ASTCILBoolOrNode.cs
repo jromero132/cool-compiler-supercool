11f1cfb0854765b0a8fe3ef0f2157ed990684a09
jyeros <jyero96@gmail.com>
2019-06-02 03:27:01 -0400

    add or node in CIL

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILBoolOrNode.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILBoolOrNode : ASTCILExpressionNode
+    {
+        public ASTCILExpressionNode Left { get; }
+        public ASTCILExpressionNode Right { get; }
+
+        public ASTCILBoolOrNode(ASTCILExpressionNode left, ASTCILExpressionNode right)
+        {
+            Left = left;
+            Right = right;
+        }
+    }
+}
