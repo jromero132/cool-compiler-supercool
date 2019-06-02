d6c5f8fac7ff63cc6069ca8cd63bd744b68c7104
Jose10go <Jose10go2017@gmail.com>
2019-06-02 03:15:56 -0400

    Adding Ensures to SemanticCheckResult

--- src/SuperCOOL/SemanticCheck/SemanticCheckResult.cs
+++ src/SuperCOOL/SemanticCheck/SemanticCheckResult.cs
@@ -1,16 +1,42 @@
 ﻿using SuperCOOL.Core;
+using System;
 using System.Collections.Generic;
 
 namespace SuperCOOL.SemanticCheck
 {
     public class SemanticCheckResult
     {
-        public bool Correct { get; internal set; }
-        public CoolType Type { get; internal set; }
+        public bool Correct => Errors.Count == 0;
+        public CoolType Type { get; private set; }
         public List<string> Errors { get; internal set; }
         public SemanticCheckResult()
         {
             Errors = new List<string>();
+        }
+
+        public void Ensure(SemanticCheckResult semanticCheckResult)
+        {
+            if (!semanticCheckResult.Correct)
+                Errors.AddRange(semanticCheckResult.Errors);
+        }
+        public void Ensure(SemanticCheckResult semanticCheckResult ,bool condition, string Message)
+        {
+            if (!semanticCheckResult.Correct)
+                Errors.AddRange(semanticCheckResult.Errors);
+            else if (!condition)
+                Errors.Add(Message);
+        }
+
+        internal void EnsureReturnType(CoolType type)
+        {
+            if (Correct)
+                Type = type;
+        }
+
+        internal void Ensure(bool condition, string Message)
+        {
+            if (!condition)
+                Errors.Add(Message);
         }
     }
 }
