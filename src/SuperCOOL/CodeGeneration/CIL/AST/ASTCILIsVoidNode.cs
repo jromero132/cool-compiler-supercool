18434aa45c2460082033ed2df8988973d76ef22b
jyeros <jyero96@gmail.com>
2019-06-02 05:31:28 -0400

    add CIL IsVoidNode

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIsVoidNode.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILIsVoidNode : ASTCILExpressionNode
+    {
+        public ASTCILExpressionNode Expression { get; }
+
+        public ASTCILIsVoidNode(ASTCILExpressionNode expression)
+        {
+            Expression = expression;
+        }
+    }
+}
