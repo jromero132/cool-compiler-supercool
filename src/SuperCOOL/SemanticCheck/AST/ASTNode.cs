28059d965469131fd80aa061ad874b1a43dc25ae
Jose10go <Jose10go2017@gmail.com>
2019-06-02 04:29:59 -0400

    Adding SemanticCheckResult to ASTNode

--- src/SuperCOOL/SemanticCheck/AST/ASTNode.cs
+++ src/SuperCOOL/SemanticCheck/AST/ASTNode.cs
@@ -4,11 +4,8 @@ namespace SuperCOOL.SemanticCheck.AST
 {
     public abstract class ASTNode
     {
-        protected ASTNode()
-        {
-            TypeEnvironment = new TypeEnvironment();
-        }
         public TypeEnvironment TypeEnvironment { get; set; }
+        public SemanticCheckResult SemanticCheckResult { get; set; }
         public virtual Result Accept<Result>(ISuperCoolASTVisitor<Result> Visitor)
         {
             return Visitor.Visit(this);
