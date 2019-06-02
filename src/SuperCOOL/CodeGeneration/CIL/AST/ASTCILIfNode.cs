01cd5aa85533f1d51c57de96087f6cc7a98fa5af
jyeros <jyero96@gmail.com>
2019-06-01 23:51:19 -0400

    change CIL If node

--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIfNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIfNode.cs
@@ -3,11 +3,16 @@
     class ASTCILIfNode : ASTCILNode
     {
         public ASTCILExpressionNode Condition { get; }
+        public ASTCILExpressionNode Then { get; }
+        public ASTCILExpressionNode Else { get; }
         public string Label { get; }
 
-        public ASTCILIfNode(ASTCILExpressionNode condition, string label)
+        public ASTCILIfNode(ASTCILExpressionNode condition, ASTCILExpressionNode then, ASTCILExpressionNode @else,
+            string label)
         {
             Condition = condition;
+            Then = then;
+            Else = @else;
             Label = label;
         }
     }
