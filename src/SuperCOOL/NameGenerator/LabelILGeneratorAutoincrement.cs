ae2fe0680c0c806ff15bc06f36b8acc00dff2b20
jyeros <jyero96@gmail.com>
2019-06-01 23:50:34 -0400

    add ILabelILGenerator

---
+++ src/SuperCOOL/NameGenerator/LabelILGeneratorAutoincrement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCOOL.NameGenerator
+{
+    class LabelILGeneratorAutoincrement : ILabelILGenerator
+    {
+        private int ifIndex { get; set; }
+        private int whileIndex { get; set; }
+        public LabelILGeneratorAutoincrement()
+        {
+            ifIndex = whileIndex = 0;
+        }
+        public string GenerateIf()
+        {
+            return $"if_{ifIndex++}";
+        }
+
+        public string GenerateWhile()
+        {
+            return $"while_{whileIndex++}";
+        }
+    }
+}
