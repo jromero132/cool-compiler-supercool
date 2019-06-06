33352bf3be98766a7ed8e7408de7ce8f74b9bd59
jyeros <jyero96@gmail.com>
2019-06-05 23:38:38 -0400

    add runtime error id

--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILRuntimeErrorNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILRuntimeErrorNode.cs
@@ -1,6 +1,13 @@
-﻿namespace SuperCOOL.CodeGeneration.CIL.AST
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
 {
     class ASTCILRuntimeErrorNode : ASTCILExpressionNode
     {
+        public int Id { get; }
+        public ASTCILRuntimeErrorNode(int id)
+        {
+            Id = id;
+        }
     }
 }
