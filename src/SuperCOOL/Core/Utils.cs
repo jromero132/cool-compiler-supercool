1ebf20235930e3e615d0c1d223e240e36481afac
Jose10go <Jose10go2017@gmail.com>
2019-06-06 05:10:13 -0400

    Utils Extensions

---
+++ src/SuperCOOL/Core/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCOOL.Core
+{
+    public static class Utils
+    {
+        public static bool IsSelf(this string name)
+        {
+            return name == "self";//TODO : me parece !!!
+        }
+
+        public static bool IsSelfType(this string name)
+        {
+            return name == "SelfType";
+        }
+    }
+}
