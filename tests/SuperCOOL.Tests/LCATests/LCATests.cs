35d6585d671fa5e9fa2e81aed80e80d67817ff70
Jose10go <Jose10go2017@gmail.com>
2019-06-07 03:15:05 -0400

    Unit Test Working again

--- tests/SuperCOOL.Tests/LCATests/LCATests.cs
+++ tests/SuperCOOL.Tests/LCATests/LCATests.cs
@@ -11,21 +11,30 @@ namespace SuperCOOL.Tests.LCATests
         [Fact]
         public void LCATest()
         {
-            Assert.Equal( DataTypes.cu.Object, DataTypes.cu.GetTypeLCA(DataTypes.cu.Object, DataTypes.cu.Object) );
-            Assert.Equal( DataTypes.cu.Object, DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("c")));
-            Assert.Equal( DataTypes.cu.Object, DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("m")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeIfDef("i")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("m")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("n")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("q")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("p")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("n"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("p"), DataTypes.cu.GetTypeIfDef("q")));
-            Assert.Equal( DataTypes.cu.Object, DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("f"), DataTypes.cu.GetTypeIfDef("h")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("j")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("k")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("h")));
-            Assert.Equal( DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeIfDef("k")));
-            Assert.Equal( DataTypes.cu.Object, DataTypes.cu.GetTypeLCA(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeIfDef("p")));
+            Assert.Equal(DataTypes.cu.TypeEnvironment.Object, DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.TypeEnvironment.Object, DataTypes.cu.TypeEnvironment.Object));
+            Assert.Equal(DataTypes.cu.TypeEnvironment.Object, DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("c")));
+            Assert.Equal(DataTypes.cu.TypeEnvironment.Object, DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("m")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeIfDef("i")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("m")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("n")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("q")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("c"), DataTypes.cu.GetTypeIfDef("p")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("n"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("p"), DataTypes.cu.GetTypeIfDef("q")));
+            Assert.Equal(DataTypes.cu.TypeEnvironment.Object, DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("f"), DataTypes.cu.GetTypeIfDef("h")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("j")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("k")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("b"), DataTypes.cu.GetTypeIfDef("h")));
+            Assert.Equal(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeIfDef("k")));
+            Assert.Equal(DataTypes.cu.TypeEnvironment.Object, DataTypes.cu.TypeEnvironment.GetTypeLCA(DataTypes.cu.GetTypeIfDef("g"), DataTypes.cu.GetTypeIfDef("p")));
+        }
+
+    }
+    public static class Ext
+    {
+        public static CoolType GetTypeIfDef(this CompilationUnit cp, string typeName)
+        {
+            cp.TypeEnvironment.GetTypeDefinition(typeName, null, out var result);
+            return result;
         }
     }
 }
