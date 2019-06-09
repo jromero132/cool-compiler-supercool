ed7d33dfcc7a0338c49966e6cf59fa6e0c6c5b2e
jyeros <jyero96@gmail.com>
2019-06-08 20:20:34 -0400

    IO functions AST node

--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIOInStringNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIOInStringNode.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Linq;
+using SuperCOOL.Constants;
 
 namespace SuperCOOL.CodeGeneration.CIL.AST
 {
     class ASTCILIOInStringNode : ASTCILFuncNode
     {
-        public ASTCILIOInStringNode() : base("", // TODO set func name
+        public ASTCILIOInStringNode(ILabelILGenerator labelIlGenerator) : base(labelIlGenerator.GenerateFunc(Types.IO,Functions.InString),
             Enumerable.Empty<ASTCILExpressionNode>())
         {
             throw new NotImplementedException();
