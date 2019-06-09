6571dd28ccc178856f8f74dcb6b3c872bc8aa4da
jyeros <jyero96@gmail.com>
2019-06-08 22:09:06 -0400

    bugfix: body should be empty

--- src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIOOutStringNode.cs
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILIOOutStringNode.cs
@@ -1,12 +1,13 @@
-﻿using SuperCOOL.Constants;
+using System.Linq;
+using SuperCOOL.Constants;
 using SuperCOOL.Core;
 
 namespace SuperCOOL.CodeGeneration.CIL.AST
 {
     public class ASTCILIOOutStringNode : ASTCILFuncNode
     {
-        public ASTCILIOOutStringNode( ILabelILGenerator labelIlGenerator, ASTCILExpressionNode expresion, ISymbolTable symbolTable) : base( labelIlGenerator.GenerateFunc( Types.IO, Functions.OutString ),
-            new[] { expresion }, symbolTable )
+        public ASTCILIOOutStringNode( ILabelILGenerator labelIlGenerator, ISymbolTable symbolTable) : base( labelIlGenerator.GenerateFunc( Types.IO, Functions.OutString ),
+            Enumerable.Empty<ASTCILExpressionNode>(), symbolTable)
         {
         }
 
