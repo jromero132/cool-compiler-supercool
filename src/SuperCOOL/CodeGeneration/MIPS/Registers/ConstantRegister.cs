887d4ad5b92dcadfa86e10b2750667a8a8b4ea69
Jose Ariel Romero <josea132.romero@gmail.com>
2019-06-09 05:42:55 -0400

    Added ConstantRegister class to allow registers with constant values.

---
+++ src/SuperCOOL/CodeGeneration/MIPS/Registers/ConstantRegister.cs
@@ -0,0 +1,7 @@
+﻿namespace SuperCOOL.CodeGeneration.MIPS.Registers
+{
+    public class ConstantRegister : Register
+    {
+        public ConstantRegister( string syntax ) : base( syntax, "Constant Register" ) { }
+    }
+}
