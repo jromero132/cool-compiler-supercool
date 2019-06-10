9991baee57428d67026d135235a2c8047d3d9cad
jyeros <jyero96@gmail.com>
2019-06-10 00:19:15 -0400

    add IL string nodes

---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILStringConcatNode.cs
@@ -0,0 +1,15 @@
+﻿using SuperCOOL.Constants;
+using SuperCOOL.Core;
+using System.Linq;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    public class ASTCILStringConcatNode : ASTCILFuncNode
+    {
+        public ASTCILStringConcatNode(ILabelILGenerator labelIlGenerator, ISymbolTable symbolTable) : base(
+            labelIlGenerator.GenerateFunc(Types.String, Functions.Concat), Enumerable.Empty<ASTCILExpressionNode>(),
+            symbolTable)
+        {
+        }
+    }
+}
---
+++ src/SuperCOOL/CodeGeneration/CIL/AST/ASTCILStringSubStrNode.cs
@@ -0,0 +1,15 @@
+﻿using SuperCOOL.Constants;
+using SuperCOOL.Core;
+using System.Linq;
+
+namespace SuperCOOL.CodeGeneration.CIL.AST
+{
+    class ASTCILStringSubStrNode : ASTCILFuncNode
+    {
+        public ASTCILStringSubStrNode(ILabelILGenerator labelIlGenerator, ISymbolTable symbolTable) : base(
+            labelIlGenerator.GenerateFunc(Types.String, Functions.Substr), Enumerable.Empty<ASTCILExpressionNode>(),
+            symbolTable)
+        {
+        }
+    }
+}
