b4ba0246fff0942e58ce3d77195fb5ae7568ee6e
jyeros <jyero96@gmail.com>
2019-06-17 18:42:39 -0400

    add attributes to Int and Bool boxed

--- src/SuperCOOL/Core/Constants/Attributes.cs
+++ src/SuperCOOL/Core/Constants/Attributes.cs
@@ -3,6 +3,8 @@
     public static class Attributes
     {
         public const string StringValue = "_value";
+        public const string BoolValue = "_value";
+        public const string IntValue = "_value";
         public const string StringLength = "_length";
     }
 }
