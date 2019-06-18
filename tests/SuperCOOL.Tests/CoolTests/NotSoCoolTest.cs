88078d2e51ba615ff9f5db2f7499f164f2499ad2
Jose10go <Jose10go2017@gmail.com>
2019-06-18 06:29:45 -0400

    adding notSoCoolTests

---
+++ tests/SuperCOOL.Tests/CoolTests/NotSoCoolTest.cs
@@ -0,0 +1,34 @@
+﻿using SuperCOOL.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace SuperCOOL.Tests.CoolTests
+{
+    public class NotSoCoolTest
+    {
+        [Fact]
+        public void  CyclicalInheritance()
+        {
+            var errors = Compiler.Compile(new[] { Path.Combine("Examples", "NotSoCool", "cyclical_inheritance.cl")}, out string code, out var limits);
+            Assert.Contains(new Error("Detected Cyclical Inheritance", ErrorKind.SemanticError), errors,new MessageErrorComparer());
+        }
+
+        class MessageErrorComparer : IEqualityComparer<Error>
+        {
+            public bool Equals(Error x, Error y)
+            {
+                return x.Message == y.Message;
+            }
+
+            public int GetHashCode(Error obj)
+            {
+                return obj.Message.GetHashCode();
+            }
+        }
+
+      
+    }
+}
---
+++ tests/SuperCOOL.Tests/Examples/NotSoCool/cyclical_inheritance.cl
@@ -0,0 +1,19 @@
+Class Main inherits IO
+{
+    main():Object
+    {
+        "nothing"
+    };
+};
+class A inherits B
+{
+
+};
+class B inherits C
+{
+
+};
+class C inherits A
+{
+
+};
